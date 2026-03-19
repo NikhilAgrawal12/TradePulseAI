d34f31383039f4bba019e22814943c2801cb53d5
Nikhil Agrawal <nikhilagrawal1289@gmail.com>
2026-03-19 07:46:17 -0400

    Create Customer entity

--- backend/user-service/src/main/java/com/pm/userservice/dto/validators/CreateUserValidationGroup.java
+++
@@ -1,4 +0,0 @@
-package com.pm.userservice.dto.validators;
-
-public interface CreateUserValidationGroup {
-}
---
+++ tradepulseai-backend/cust-service/src/test/java/com/tradepulseai/custservice/CustServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradepulseai.custservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
