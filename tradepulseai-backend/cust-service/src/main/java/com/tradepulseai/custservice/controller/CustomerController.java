ba25c3bea03dc7d378cd880954773b5b002717ae
Nikhil Agrawal <nikhilagrawal1289@gmail.com>
2026-03-21 10:03:12 -0400

    Add api documentation

--- tradepulseai-backend/cust-service/src/main/java/com/tradepulseai/custservice/controller/CustomerController.java
+++ tradepulseai-backend/cust-service/src/main/java/com/tradepulseai/custservice/controller/CustomerController.java
@@ -4,6 +4,8 @@ import com.tradepulseai.custservice.dto.CustomerRequestDTO;
 import com.tradepulseai.custservice.dto.CustomerResponseDTO;
 import com.tradepulseai.custservice.dto.validators.CreateCustomerValidationGroup;
 import com.tradepulseai.custservice.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/customers")
+@Tag(name="Customers", description = "API for managing customers")
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -22,25 +25,30 @@ public class CustomerController {
         this.customerService = customerservice;
     }
 
+
     @GetMapping
+    @Operation(summary="Get customers")
     public ResponseEntity<List<CustomerResponseDTO>> getCustomers() {
         List<CustomerResponseDTO> users = customerService.getCustomers();
         return ResponseEntity.ok().body(users);
     }
 
     @PostMapping
+    @Operation(summary="Create a new customer")
     public ResponseEntity<CustomerResponseDTO> createUser(@Validated({Default.class, CreateCustomerValidationGroup.class}) @RequestBody CustomerRequestDTO customerRequestDTO){
         CustomerResponseDTO custResponseDTO = customerService.createCustomer(customerRequestDTO);
         return ResponseEntity.ok().body(custResponseDTO);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary="Update customer")
     public ResponseEntity<CustomerResponseDTO>  updateUser(@PathVariable UUID id, @Validated({Default.class}) @RequestBody CustomerRequestDTO customerRequestDTO){
         CustomerResponseDTO custResponseDTO = customerService.updateCustomer(id, customerRequestDTO);
         return ResponseEntity.ok().body(custResponseDTO);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary="Delete customer")
     public ResponseEntity<Void>  deleteUser(@PathVariable UUID id){
         customerService.deleteCustomer(id);
         return ResponseEntity.noContent().build();
