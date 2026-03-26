f1382107f0a83015564d62d1c60a8e0652564ddf
Nikhil Agrawal <nikhilagrawal1289@gmail.com>
2026-03-26 14:12:00 -0400

    Create AuthService

---
+++ tradepulseai-backend/auth-service/src/main/java/com/tradepulseai/authservice/service/AuthService.java
@@ -0,0 +1,38 @@
+package com.tradepulseai.authservice.service;
+
+import com.tradepulseai.authservice.dto.LoginRequestDTO;
+import com.tradepulseai.authservice.model.User;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.JwtException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static org.hibernate.Hibernate.map;
+
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(),u.getRole()));
+
+        return token;
+    }
+
+
+}
